56f183f58521d22d4582c960c2edcd9564bfb50e
Leon Shimizu <lmshimizu@gmail.com>
2025-05-20 11:29:50 +1000

    Update the create and update methods to use the show jbuilder view/template

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,31 +12,31 @@ class StudentsController < ApplicationController
   end
 
   def create
-    student = Student.create(
+    @student = Student.create(
       first_name: params["first_name"],
       last_name: params["last_name"],
       subject: params["subject"]
-    )
+  )
 
-      render json: student
-    end
+    render :show
+  end
 
   def update
-    student = Student.find(params["id"])
+    @student = Student.find(params["id"])
 
-    student.update(
-      first_name: params["first_name"] || student.first_name,
-      last_name: params["last_name"] || student.last_name,
-      subject: params["subject"] || student.subject
+    @student.update(
+      first_name: params["first_name"] || @student.first_name,
+      last_name: params["last_name"] || @student.last_name,
+      subject: params["subject"] || @student.subject
     )
 
-    render json: student
+    render :show
   end
 
   def destroy
-    student = Student.find(params["id"])
+    @student = Student.find(params["id"])
 
-    student.destroy
+    @student.destroy
 
     render json: { message: "Student deleted" }
   end
